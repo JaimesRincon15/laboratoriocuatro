d6e62f48b1f4820038f87f71a66e5394222f20b7
your name <you@example.com>
2024-09-10 20:25:16 -0500

    mi cuarto proyecto

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        for (int i = 1; i <=4; i++)
+        {
+            Console.WriteLine("Los numeros del 1 al 4 son:" +i);
+        }
+    }
+}
